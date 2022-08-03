b06d75f513114d1f380961f9504d56c564ea8094
thayanasantos <thayanasistemas@gmail.com>
2022-08-03 20:03:30 -0300

    criando class livro

--- src/br/com/br/PrimeiroPrograma.java
+++ src/br/com/br/PrimeiroPrograma.java
@@ -7,8 +7,44 @@ public class PrimeiroPrograma {
 
         Gato gato =new Gato();
         System.out.println(gato);
+        Livro livro1 = new Livro("os três porkinhos", 200);
+        System.out.println(livro1);
      //   int a = 2;
        // int b = 3;
         //System.out.println("Hello Word" + (a+b));
     }
+}
+class Livro {
+    private String nome;
+    private Integer numPag;
+
+    public Livro(String nome, Integer numPag) {
+        this.nome = nome;
+        this.numPag = numPag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(Integer numPag) {
+        this.numPag = numPag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPag=" + numPag +
+                '}';
+    }
+
 }
